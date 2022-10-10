436aac2334a9f01d595a098489ac56fbb14fe645
sivasankar <sankarreddy.soft@gmail.com>
2022-10-10 15:10:14 +0530

    Eurekha Naming Server

---
+++ src/test/java/com/tech/microservice/eurekhanamingserver/TechEurekhaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.microservice.eurekhanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechEurekhaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
